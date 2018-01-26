92712e6f47a62e1b09b6bfba740e54c105d31e11
Kyle Knoepfel <knoepfel@fnal.gov>
2018-01-26 10:57:33 -0600

    Remove support for deprecated function.

--- cetlib_except/demangle.h
+++ cetlib_except/demangle.h
@@ -4,37 +4,22 @@
 // ======================================================================
 //
 // demangle: Call the cross-vendor API to demangle symbols
-//           (eg for ROOT dictionary errors).
 //
 // ======================================================================
 
 #include <string>
-
-#define DEMANGLE_MSG                                                           \
-  "\n\n"                                                                       \
-  "cetlib warning: The 'cet::demangle(std::string const&)' function is "       \
-  "deprecated.\n"                                                              \
-  "                Please use 'cet::demangle_symbol(std::string const&)'.\n\n"
 
 namespace cet {
   std::string demangle_symbol(std::string const& symbol);
   std::string demangle_symbol(char const* symbol);
 
   std::string demangle_message(std::string mangled);
-
-  [[deprecated(DEMANGLE_MSG)]] std::string demangle(std::string const& mangled);
 }
 
 inline std::string
 cet::demangle_symbol(std::string const& symbol)
 {
   return demangle_symbol(symbol.c_str());
-}
-
-inline std::string
-cet::demangle(std::string const& mangled)
-{
-  return demangle_symbol(mangled);
 }
 
 #undef DEMANGLE_MSG
