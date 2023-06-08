9d6861b3eb563095bf7a391aaa38f170da6243c1
Chris Green <greenc@fnal.gov>
2023-06-08 16:53:47 -0500

    Improve portability

--- cetlib_except/cxx20_macros.h
+++ cetlib_except/cxx20_macros.h
@@ -4,7 +4,10 @@
 #define CET_CXX20_AVAILABLE (__cplusplus >= 202002L)
 #endif
 #ifndef CET_CONCEPTS_AVAILABLE
-#define CET_CONCEPTS_AVAILABLE \
-  (defined(__cpp_concepts) && __cpp_concepts >= 202002L)
+#if defined(__cpp_concepts)
+#define CET_CONCEPTS_AVAILABLE (__cpp_concepts >= 202002L)
+#else
+#define CET_CONCEPTS_AVAILABLE 0
+#endif
 #endif
 #endif /* cetlib_except_cxx20_macros_h */
