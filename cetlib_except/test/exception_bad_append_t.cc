4048f4c9252b340693119839c2d47ec3e6ddd5f2
Chris Green <greenc@fnal.gov>
2023-08-09 17:43:03 -0500

    Replace the compile-time append-ability test with a runtime test

--- cetlib_except/test/exception_bad_append_t.cc
+++ cetlib_except/test/exception_bad_append_t.cc
@@ -1,13 +1,27 @@
+#include <catch2/catch_test_macros.hpp>
+
 #include "cetlib_except/exception.h"
-using cet::exception;
+
+#include <concepts>
+#include <ostream>
 
 namespace {
+  struct Streamable {
+    int i;
+  };
+  std::ostream&
+  operator<<(std::ostream& os, Streamable const& s)
+  {
+    return os << s.i;
+  }
   struct UnStreamable {};
+  template <typename T>
+  concept can_append_to_exception =
+    requires(cet::exception&& e, T const& t) { e.append(t); };
 }
 
-int
-main()
+TEST_CASE("Ensure streamable exception append")
 {
-  exception e("Hello");
-  e << UnStreamable{};
+  REQUIRE(can_append_to_exception<Streamable>);
+  REQUIRE_FALSE(can_append_to_exception<UnStreamable>);
 }
